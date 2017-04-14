2ccd74009ff3c8213febbd959dedf61b7f9343f7
zhangyugehu <1021091945@qq.com>
2017-04-14 18:04:42 +0800

    1、自定义NavigationBar封装,添加返回按钮和右上方文字按钮 2、设置style使安卓实现沉浸式 3、自定义标签加入编写

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # GitHubPopularProject
-react-native练手项目
+> react-native练手项目
+
+# 笔记
+
+### props 
+> 组件的属性：用于父控件给子控件穿传递数据
+
+#### 默认属性 `defaultProps`
+> 给组件添加默认属性
+
+````
+static defaultProps={
+    prop1:value,
+    prop2:value
+}
+````
+
+#### 属性检查 `propTypes`
+> 对传入的属性进行属性检查，需要导入 `PropsTypes` 
+
+````
+static propsTypes={
+    prop1:PropTypes.string,
+    prop2:PropsTypes.int
+}
+````
+
+### ref使用
+> 通过给组件设置特定的ref值，从而在代码中可以通过ref获取组件对象
+
--- js/pages/mine/CustomTabPage.js
+++ js/pages/mine/CustomTabPage.js
@@ -7,13 +7,40 @@ import {
     StyleSheet,
     Text,
     View,
-    Image
+    Image,
+    ToastAndroid
 } from 'react-native';
+
+import NavigationBar from '../../components/NavigationBar'
 
 export default class CustomTabPage extends Component{
     render(){
-        return <View>
+        return <View style={styles.container}>
+            <NavigationBar
+                barTitle="自定义标签"
+                search={false}
+                menu={false}
+                right={true}
+                rightText="保存"
+                left={true}
+                backPress={this.backPress.bind(this)}
+                rightPress={this.rightPress.bind(this)}
+            />
             <Text>CustomTabPage</Text>
         </View>
     }
+
+    backPress=()=>{
+        this.props.navigator.pop();
+    }
+    rightPress=()=>{
+        ToastAndroid.show('rightPress', ToastAndroid.SHORT);
+    }
+
 }
+
+const styles = StyleSheet.create({
+   container:{
+       flex:1,
+   },
+});
